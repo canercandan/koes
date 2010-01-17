998b50666f160ff2eeb9de392b302085f43ce93c
xiakangqi <xiakangqi@2afa99c8-fc69-11de-b5bc-ffd2847a4b57>
2010-01-17 20:39:27 +0000

    +or operation +xor operation +add operation +truth_value +bool_expression.

--- src/main.cpp
+++ src/main.cpp
@@ -218,51 +218,48 @@ static Rule*	get_a_concluding_rule_and_remove_rule(Fact F)
 
 typedef std::vector<Fact>	FactsVector;
 
-static void	condition(FactsVector& facts_to_check, Rule* R)
-{
-  (void)facts_to_check;
+// static void	condition(FactsVector& facts_to_check, Rule* R)
+// {
+//   (void)facts_to_check;
 
-  if (R->op == FACT)
-    return;
+//   if (R->op == FACT)
+//     return;
+// }
+static int	bool_expression(Node* exp);
+static int	truth_value(Fact);
+static int	fire_ability(Rule*);
+
+static int	fire_ability(Rule* R)
+{
+  int		res;
+
+  res = bool_expression(R->left);
+  return (res);
 }
 
-static bool	truth_value(Fact);
-static bool	fire_ability(Rule*);
-
-static bool	fire_ability(Rule* R)
+static int	truth_value(Fact F)
 {
-  FactsVector	facts_to_check;
-  condition(facts_to_check, R);
-
-  for (FactsVector::iterator it = facts_to_check.begin(), end = facts_to_check.end();
-       it != end; ++it)
-    {
-      Fact	F = *it;
-      if (truth_value(F) == false)
-	return false;
-      facts_to_check.erase(it);
-    }
-  return true;
-}
-
-static bool	truth_value(Fact F)
-{
-  if (facts.find(F) != facts.end())
-    return facts[F];
-
   Rule*	rule;
+  if (facts[F] == true)
+    return (1);
+  else if(facts[F] == false)
+    return (0);
+  
   while ((rule = get_a_concluding_rule_and_remove_rule(F)) != NULL)
     {
-      if (fire_ability(rule) == true)
+      if (fire_ability(rule) == 1)
 	{
-	  delete rule;
-	  return true;
+	  facts[F] = true;
+	  return 1;
 	}
-      delete rule;
+      else if (fire_ability(rule) == 0)
+	{
+	  facts[F] = false;
+	  return 0;
+	}
     }
-
-  return false;
-}
+  return -1;
+ }
 
 int	main(int ac, char** av)
 {
@@ -288,16 +285,93 @@ int	main(int ac, char** av)
        it != end; ++it)
     print_out_binary_tree(*it);
 
-  StringVector	ws = vm["wish"].as<StringVector>();
-  for (StringVector::iterator it = ws.begin(), end = ws.end();
-       it != end; ++it)
-    {
-      std::cout << "WISH [" << *it << "] = ["
-		<< std::boolalpha << truth_value(*it)
-		<< "]" << std::endl;
-    }
+  // StringVector	ws = vm["wish"].as<StringVector>();
+  // for (StringVector::iterator it = ws.begin(), end = ws.end();
+  //      it != end; ++it)
+  //   {
+  //     std::cout << "WISH [" << *it << "] = ["
+  // 		<< std::boolalpha << truth_value(*it)
+  // 		<< "]" << std::endl;
+  //   }
 
+  std::string a;
+  std::cin >> a;
+  std::cout << "target FACT: "<< a <<std::endl;
+  std::cout << "reslut" << truth_value(a) << std::endl;
   delete_rules();
 
   return 0;
 }
+static int	xor_operation(int a, int b);
+static int	or_operation(int a, int b);
+static int	add_operation(int a, int b);
+static int	operation(OperatorEnum op, int a, int b);
+//1:true, 0:false, -1:undertermind
+static int	bool_expression(Node* exp)
+{
+  int		res;
+
+  if (exp->op == FACT)
+    return (truth_value(exp->data));
+  else
+    if (exp->right->op == FACT)
+      res = operation(exp->op, truth_value(exp->left->data), truth_value(exp->right->data));
+    else 
+      res = operation(exp->op, truth_value(exp->left->data), bool_expression(exp->right));
+  return (res);
+}
+
+
+static int	operation(OperatorEnum op, int a, int b)
+{
+  int		res;
+
+  switch(op)
+    {
+    case AND:
+      res = add_operation(a, b);
+      break;
+    case OR:
+      res = or_operation(a, b);
+      break;
+    case XOR:
+      res = xor_operation(a, b);
+      break;
+    default:
+      fprintf(stderr, "invalid operator\n");
+      exit(0);
+    }
+  return (res);
+}
+
+static int	add_operation(int a, int b)
+{
+  if (a && b)
+    return (1);
+  else if (a == 0 || b == 0)
+    return (0);
+  return (-1);
+}
+
+static int	or_operation(int a, int b)
+{
+  if (a || b)
+    return (1);
+  else if (a == 0 && b == 0)
+    return (0);
+  return (-1);
+}
+
+static int	xor_operation(int a, int b)
+{
+  int		r;
+
+  r = or_operation(a, b);
+  if (r == 1)
+    return (0);
+  else if (r == 0)
+    return (1);
+  return (-1);
+}
+
+
