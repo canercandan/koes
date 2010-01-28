167f6f4f9964f09e93b0a569137f5e98835fa4d8
xiakangqi <xiakangqi@2afa99c8-fc69-11de-b5bc-ffd2847a4b57>
2010-01-28 01:10:29 +0000

    +ask user questions when there is no answer.

--- src/main.cpp
+++ src/main.cpp
@@ -56,15 +56,11 @@ static void	parse_command(std::string& cmd)
 
 int	main(int ac, char** av)
 {
-  //! parsing phase for options files
   options_parsing(ac, av);
   files_parsing();
-
-  //! parsing for --true and --false options
   facts_parsing(true);
   facts_parsing(false);
 
-  //! push asked facts by wish option to g_wishes
   if (g_vm.count("wish") && g_vm.count("filename"))
     {
       StringVector	ws = g_vm["wish"].as<StringVector>();
@@ -73,8 +69,6 @@ int	main(int ac, char** av)
       	g_wishes.push_back(*it);
     }
 
-  //! if there is a fact asked from the both file and option
-  //! then we call print_result for each of them
   if (g_wishes.size() > 0 && g_vm.count("filename"))
     {
       for (StringVector::iterator it = g_wishes.begin(), end = g_wishes.end();
@@ -85,7 +79,7 @@ int	main(int ac, char** av)
 	  g_fired_rules.clear();
 	}
     }
-  else //! otherwise we go in interactive mode
+  else
     {
       std::cout << "CTRL+C to quit or type ? for help" << std::endl;
       while (1)
@@ -98,7 +92,6 @@ int	main(int ac, char** av)
 	  g_fired_rules.clear();
 	}
     }
-  //! to finish we free all allocated memory
   delete_rules();
   return 0;
 }
--- src/print_result.cpp
+++ src/print_result.cpp
@@ -1,16 +1,87 @@
 #include <iostream>
 #include "functions.h"
 #include "globals.h"
+#include "sets.h"
+StringVector	fired_facts;
+
+static bool	fact_is_in_fired_facts(Fact fact)
+{
+  for (StringVector::iterator it = fired_facts.begin(), end = fired_facts.end();
+       it != end; ++it)
+    {
+      if ((*it) == fact)
+	return true;
+    }
+  return false;
+}
+
+
+static bool	fact_is_in_g_facts(Fact fact)
+{
+  if (g_facts.find(fact) != g_facts.end())
+    return true;
+  return false;
+}
+
+static void	get_facts_from_node(Node *node)
+{
+  if (node->op == FACT)
+    {
+      if (!fact_is_in_fired_facts(node->data) && !fact_is_in_g_facts(node->data))
+	fired_facts.push_back(node->data);
+    }
+  else
+    {
+      get_facts_from_node(node->left);
+      get_facts_from_node(node->right);
+    }
+  return ;
+}
+
+static void	get_facts_from_fired_rules()
+{
+  for (RulesSet::reverse_iterator it = g_fired_rules.rbegin(), end = g_fired_rules.rend();
+       it != end; ++it)
+    {
+      get_facts_from_node((*it)->left);
+      get_facts_from_node((*it)->right);
+    }
+  return ;
+}
+
+static void		ask_question()
+{
+  get_facts_from_fired_rules();
+  for (StringVector::iterator it = fired_facts.begin(), end = fired_facts.end();
+       it != end; ++it)
+    {
+      std::string	b_str;
+      if (*it == "")
+	continue;
+      std::cout << "what is the fact?[true|false]" << std::endl;
+      std::cout << "? " << *it << "=";
+      std::cin >> b_str;
+      if (b_str == "true")
+	g_facts[*it] = true;
+      else if (b_str == "false")
+	g_facts[*it] = false;
+      break;
+    }
+  fired_facts.clear();
+  g_used_rules.clear();
+  g_fired_rules.clear();
+}
 
 void	print_result(Fact F)
 {
-  tribool	res = truth_value(F);
-  //  while (res == unknown)
-  // question_parsing();
+  tribool	res;
+ 
+  while (indeterminate(res = truth_value(F)))
+    ask_question();
   std::cout << "Searching for " << F << " = " << bool_to_string(res) << std::endl;
   if (g_vm.count("verbose") && g_vm["verbose"].as<int>() >= 1)
     {
-      print_out_fired_rules();
+      print_out_fired_rules();     
       print_out_facts_table();
     }
   g_facts = g_initial_facts;
